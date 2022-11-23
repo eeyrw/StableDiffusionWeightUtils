62f65226557f5c7e2cfad9eb4f494a54b5250786
aaa <bbb>
2022-11-23 16:25:56 +0800

    M

--- DataComparer.py
+++ DataComparer.py
@@ -1,4 +1,3 @@
-import xml.etree.ElementTree as ET
 import os
 import sys
 from terminaltables import AsciiTable
@@ -8,77 +7,105 @@ import traceback
 
 
 class DataComparer(object):
-    def __init__(self,dataRecord):
-        #dataRecord=[{'name':[record name],
+    def __init__(self, dataRecord):
+        # dataRecord=[{'name':[record name],
         # 'source:[record source path],
         # 'sourceFriendlyName':[Record source simple name],
         # 'dataDict':[Record field versus value dict]},...]
-        self.compareFieldList=[]
-        self.dataRecord=dataRecord
-        self.checkResultListDict={}
-        self.keepFullCompareResult=False
+        self.compareFieldList = []
+        self.dataRecord = dataRecord
+        self.checkResultListDict = {}
+        self.keepFullCompareResult = False
         return
 
-    def customCompareFieldListList(self,compareFieldList):
-        self.compareFieldList=compareFieldList
+    def customCompareFieldListList(self, compareFieldList):
+        self.compareFieldList = compareFieldList
 
-    def setKeepFullCompareResultFlag(self,flag):
-        self.keepFullCompareResult=flag            
+    def setKeepFullCompareResultFlag(self, flag):
+        self.keepFullCompareResult = flag
 
     def getShortExceptionInfo(self):
         exc_type, exc_value, exc_tb = sys.exc_info()
         tbe = traceback.TracebackException(exc_type, exc_value, exc_tb)
         return list(tbe.format_exception_only())[-1]
-         
-    def diffRecord(self,recordName,recordList):
-        affectedFields=set()
-        allFieldsList=list(reduce(lambda x,y :x|y,[set(item['dataDict'].keys()) for item in recordList]))
-        currentCompareFieldList=[]
-        tableData=[]
-        tableData.append(['FieldName','Source', 'Value'])
 
-        if self.compareFieldList==[]:
-            currentCompareFieldList=allFieldsList
+    def diffRecord(self, recordName, recordList):
+        affectedFields = set()
+        allFieldsList = list(reduce(lambda x, y: x | y, [set(
+            item['dataDict'].keys()) for item in recordList]))
+        currentCompareFieldList = []
+        tableData = []
+        tableData.append(['FieldName', 'Source', 'Value'])
+
+        if self.compareFieldList == []:
+            currentCompareFieldList = allFieldsList
         else:
-            currentCompareFieldList=list(set(allFieldsList)&set(self.compareFieldList))
+            currentCompareFieldList = list(
+                set(allFieldsList) & set(self.compareFieldList))
 
-
-        diffCount=0
+        diffCount = 0
         for compareField in currentCompareFieldList:
-            valueItemDict={}
+            valueItemDict = {}
             for record in recordList:
-                recordDataDict=record['dataDict']
+                recordDataDict = record['dataDict']
                 if compareField in recordDataDict.keys():
-                    fieldValue=recordDataDict[compareField]
-                    valueItemDict.setdefault(fieldValue,[]).append(record)
+                    fieldValue = recordDataDict[compareField]
+                    valueItemDict.setdefault(fieldValue, []).append(record)
                 else:
-                    valueItemDict.setdefault('MISSING',[]).append(record)
+                    valueItemDict.setdefault('MISSING', []).append(record)
 
-            if len(valueItemDict.items())>1:
-                diffCount+=1
-                for k,v in valueItemDict.items():
+            if len(valueItemDict.items()) > 1:
+                diffCount += 1
+                for k, v in valueItemDict.items():
                     affectedFields.add(compareField)
                     if self.keepFullCompareResult:
-                        valueText=k
+                        valueText = k
                     else:
-                        valueText=textwrap.shorten(k,width=35)
-                    tableData.append([compareField,reduce((lambda x, y: x+'\n'+y),[item['sourceFriendlyName'] for item in v]), valueText])              
+                        valueText = textwrap.shorten(k, width=35)
+                    tableData.append([compareField, reduce(
+                        (lambda x, y: x+'\n'+y), [item['sourceFriendlyName'] for item in v]), valueText])
 
-        table = AsciiTable(tableData)  
-        table.inner_row_border = True  
+        table = AsciiTable(tableData)
+        table.inner_row_border = True
         table.title = recordName
-        return diffCount,affectedFields,table.table,tableData      
+        return diffCount, affectedFields, table.table, tableData
 
     def compare(self):
-        diffResultTextList=[]
-        diffResultTableDataList=[]
-        variableDataRecordListDict={}
+        diffResultTextList = []
+        diffResultTableDataList = []
+        variableDataRecordListDict = {}
         for record in self.dataRecord:
-            variableDataRecordListDict.setdefault(record['name'],[]).append(record)
-        
-        for k,v in variableDataRecordListDict.items():
-            diffCount,affectedFields,diffTextResult,tableData=self.diffRecord(k,v)
-            if diffCount>0:
+            variableDataRecordListDict.setdefault(
+                record['name'], []).append(record)
+
+        for k, v in variableDataRecordListDict.items():
+            diffCount, affectedFields, diffTextResult, tableData = self.diffRecord(
+                k, v)
+            if diffCount > 0:
                 diffResultTextList.append(diffTextResult)
-                diffResultTableDataList.append((k,affectedFields,tableData))
-        return diffResultTextList,diffResultTableDataList
+                diffResultTableDataList.append((k, affectedFields, tableData))
+        return diffResultTextList, diffResultTableDataList
+
+
+if __name__ == "__main__":
+    dataRecord = [{'name': 'gfgsg',
+                  'source': 'aaa',
+                   'sourceFriendlyName': 'aa',
+                   'dataDict': {'a1': '2', 'a2': '3'}},
+                  {'name': 'gfgsg',
+                   'source': 'bbb',
+                   'sourceFriendlyName': 'bb',
+                   'dataDict': {'a1': '5', 'a2': '3', 'a3': '5'}},
+                   {'name': 'sbnbn',
+                  'source': 'aaa',
+                   'sourceFriendlyName': 'aa',
+                   'dataDict': {'a1': '2', 'a2': '3'}},
+                  {'name': 'sbnbn',
+                   'source': 'bbb',
+                   'sourceFriendlyName': 'bb',
+                   'dataDict': {'a1': '5', 'a2': '3', 'a3': '5'}}
+                   ]
+    dc = DataComparer(dataRecord)
+    txt, _ = dc.compare()
+    for t in txt:
+        print(t)
